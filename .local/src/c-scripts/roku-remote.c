e266a1c667eb0585b9579aa2ff71bf0eb11a0d42
Dusan <oalfodr@gmail.com>
2025-04-18 13:06:07 +0200

    roku remote to use libnotify

--- .local/src/c-scripts/roku-remote.c
+++ .local/src/c-scripts/roku-remote.c
@@ -1,8 +1,8 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
+#include <libnotify/notify.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <curl/curl.h>
 #include <ctype.h>
@@ -11,10 +11,10 @@ const char *roku_ip = "192.168.56.77";
 Window target_win = 0;
 int should_send_notifications = 0;
 
-void notify_event(const char *event_type, const char *key) {
-	char command[256];
-	snprintf(command, sizeof(command), "notify-send 'Roku Input' '%s: %s'", event_type, key);
-	system(command);
+void notify_event(const char *event_type, const char *message) {
+	NotifyNotification *n = notify_notification_new(event_type, message, NULL);
+	notify_notification_show(n, NULL);
+	g_object_unref(G_OBJECT(n));
 }
 
 const char* get_roku_key(KeySym keysym, XKeyEvent *ev) {
@@ -25,13 +25,12 @@ const char* get_roku_key(KeySym keysym, XKeyEvent *ev) {
 		case XK_Right:     return "right";
 		case XK_Up:        return "up";
 		case XK_Down:      return "down";
-		case XK_F1:        return "search";
 		case XK_less:      return "rev";
 		case XK_greater:   return "fwd";
 		case XK_BackSpace: return "back";
+		case XK_F1:        return "search";
 		case XK_F12:       return "backspace";
-		default:
-			break;
+		default:           break;
 	}
 
 	static char litkey[32];
@@ -54,26 +53,20 @@ int send_roku_request(KeySym keysym, XKeyEvent *ev, const char *action) {
 	if (should_send_notifications)
 		notify_event(action, key);
 
-	CURL *curl;
-	CURLcode res;
-	int success = 1;
-
 	char url[256];
 	snprintf(url, sizeof(url), "http://%s:8060/%s/%s", roku_ip, action, key);
 
-	curl = curl_easy_init();
-	if (curl) {
-		curl_easy_setopt(curl, CURLOPT_URL, url);
-		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "");
-		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 1L);
+	CURL *curl = curl_easy_init();
+	if (!curl) return 0;
 
-		res = curl_easy_perform(curl);
-		if (res != CURLE_OK) success = 0;
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "");
+	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 1L);
 
-		curl_easy_cleanup(curl);
-	}
+	CURLcode res = curl_easy_perform(curl);
+	curl_easy_cleanup(curl);
 
-	return success;
+	return (res == CURLE_OK);
 }
 
 Window select_window(Display *dpy) {
@@ -85,16 +78,19 @@ Window select_window(Display *dpy) {
 	XWindowEvent(dpy, root, ButtonPressMask, &ev);
 	XUngrabPointer(dpy, CurrentTime);
 
-	return ev.xbutton.subwindow ? ev.xbutton.subwindow : root;
+	return ev.xbutton.subwindow ? ev.xbutton.subwindow : ev.xbutton.window;
 }
 
 int main(int argc, char **argv) {
 	int opt;
-	while ((opt = getopt(argc, argv, "i:")) != -1) {
+	while ((opt = getopt(argc, argv, "i:n")) != -1) {
 		switch (opt) {
 			case 'i': roku_ip = optarg; break;
+			case 'n': should_send_notifications = 1; break;
 		}
 	}
+
+	notify_init("Roku Control");
 
 	Display *display = XOpenDisplay(NULL);
 	if (!display) return 1;
@@ -102,14 +98,15 @@ int main(int argc, char **argv) {
 
 	target_win = select_window(display);
 	notify_event("Bound", "Window selected");
+	XStoreName(display, target_win, "Listening to keyboard events");
+	XFlush(display);
 
 	Window root = DefaultRootWindow(display);
 	XSelectInput(display, root, KeyPressMask | KeyReleaseMask | PointerMotionMask);
 
 	XEvent event;
-	int key_is_held = 0;
+	int key_is_held = 0, keyboard_grabbed = 0;
 	unsigned int last_keycode = 0;
-	int keyboard_grabbed = 0;
 
 	while (1) {
 		XNextEvent(display, &event);
@@ -131,13 +128,9 @@ int main(int argc, char **argv) {
 
 		if (!over_target || !keyboard_grabbed) continue;
 
+		KeySym keysym = XLookupKeysym(&event.xkey, (event.xkey.state & ShiftMask) ? 1 : 0);
 		if (event.type == KeyPress && key_is_held == 0) {
-			KeySym keysym = XLookupKeysym(&event.xkey, (event.xkey.state & ShiftMask) ? 1 : 0);
-			if (keysym == XK_Shift_L || keysym == XK_Shift_R) {
-				continue;
-			} else if (keysym == XK_Control_L || keysym == XK_Control_R) {
-				continue;
-			}
+			if (keysym == XK_Shift_L || keysym == XK_Shift_R || keysym == XK_Control_L || keysym == XK_Control_R) continue;
 
 			key_is_held = 1;
 			last_keycode = event.xkey.keycode;
@@ -150,11 +143,13 @@ int main(int argc, char **argv) {
 			}
 		} else if (event.type == KeyRelease && key_is_held == 1 && event.xkey.keycode == last_keycode && XPending(display) == 0) {
 			key_is_held = 0;
-			KeySym keysym = XLookupKeysym(&event.xkey, 0);
 			send_roku_request(keysym, &event.xkey, "keyup");
 		}
 	}
 
+	XStoreName(display, target_win, "Roku");
+	XFlush(display);
 	XCloseDisplay(display);
+	notify_uninit();
 	return 0;
 }
