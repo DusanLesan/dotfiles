33020aea02c50ee4e81a15c08b7db6c847397fbc
Dusan <oalfodr@gmail.com>
2026-03-29 00:04:57 +0100

    clean-history

---
+++ .config/firefox/user.js
@@ -0,0 +1,4 @@
+user_pref("browser.compactmode.show", true);
+user_pref("browser.uidensity", 1);
+user_pref("browser.toolbars.bookmarks.visibility", "newtab");
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
